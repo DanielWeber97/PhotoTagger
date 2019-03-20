a1d670626cdc6eb872280388389e473f25aea35d
DanielWeber97 <daniel.weber.443@gmail.com>
2019-03-20 16:45:44 -0400

    Take picture and stores bitmap. Currently rotated 90 degrees.

--- app/src/main/java/com/example/phototagger/MainActivity.java
+++ app/src/main/java/com/example/phototagger/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.phototagger;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase db;
+    final int IMAGE_CODE = 888;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = this.openOrCreateDatabase("Database", Context.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE if not exists Photos(ID int, Photo text, Size int)");
+        db.execSQL("CREATE TABLE if not exists Tags(ID int, Tag text)");
+    }
+
+
+    public void capture(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, IMAGE_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent x) {
+        Bundle extras = x.getExtras();
+        Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+        Log.v("MYTAG", "in OnActivityResult");
+
+        ImageView img = (ImageView) findViewById(R.id.display);
+        img.setImageBitmap(imageBitmap);
     }
 }
